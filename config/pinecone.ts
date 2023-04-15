65db5e6447e0e5f8fcc928a6895bf4f0bb3bb4db
Jia Yaoqi <jiayaoqijia@gmail.com>
2023-04-15 18:05:29 +0800

    added settings

---
+++ config/pinecone.ts
@@ -0,0 +1,9 @@
+/**
+ * Change the index and namespace to your own
+ */
+
+const PINECONE_INDEX_NAME = 'solidity';
+
+const PINECONE_NAME_SPACE = 'solidity-retrieval5'; //namespace is optional for your vectors
+
+export { PINECONE_INDEX_NAME, PINECONE_NAME_SPACE };
